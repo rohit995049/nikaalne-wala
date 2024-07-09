f6fb408ed18c82238929fb3b82ed61b6d194f1ce
rohit995049 <161129526+rohit995049@users.noreply.github.com>
2024-07-09 06:31:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 21233086
-api_hash = "2892db755e485aaf8e55b4b70e8953f6"
-bot_token = "7109218345:AAHE_RTiW5-JcFJ36-vEkJNexxJm0cPjI_I"
-auth_users = "7107871207"
+api_id = 27498866
+api_hash = "96fbb6ad2e11ab04e83ca09ef3f42455"
+bot_token = "7186164956:AAEAvQf6DL46n2nfOOw_47A4Tn9bnIcmFwk"
+auth_users = "6488911325"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "7107871207"
+osowner_users = "6488911325"
 owner_users = [int(num) for num in osowner_users.split(",")]
